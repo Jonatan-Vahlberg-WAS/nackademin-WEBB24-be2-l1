da8ff291deb4efd6036f0b8473b23d2991a43689
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-09-15 10:03:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var arr = [1, 2, 3];
+console.log("Hello world!", arr.reduce(function (p, v) { return p + v; }, 0));
---
+++ index.ts
@@ -0,0 +1,2 @@
+const arr = [1,2,3]
+console.log("Hello world!", arr.reduce((p, v) => p+v,0))
