d979624ce8a378e92e387f7b1a60ea2d0ce78f7d
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-09-18 08:48:13 +0200

    move lesson code

--- index.ts
+++ index.ts
@@ -1,18 +1,1 @@
-console.log("Functions")
-
-function add(x: number, y: number): number{
-    return x+y
-}
-
-//! avoid if possible multiple returns
-function divide(x: number, y:number): number | string {
-    if(x === 0){
-        return "Zero not valid as numerator"
-    }
-    return x/y
-}
-
-//! Avoid any if possible look below for reason
-const a: any = ""
-
-//! a.times.x[0].length
+console.log("Lesson 2")
